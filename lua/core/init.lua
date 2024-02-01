ebe65c47e8fadd3ee01f73c9bab815145bd82c07
guicoelhodev <gs.coelho_dev@outlook.com>
2024-02-01 10:29:42 -0300

    feat: change harpoon mapping

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -41,7 +41,7 @@ opt.splitright = true
 opt.termguicolors = true
 opt.timeoutlen = 400
 opt.undofile = true
-
+vim.o.showtabline = 0 -- hidden tabs
 -- interval for writing swap file to disk, also used by gitsigns
 opt.updatetime = 250
 
